d8454935cfe4a01a34b5a20841e338b18d37473b
RodrigoFSouza <roferreira122@gmail.com>
2020-03-16 20:30:49 -0300

    Iniciando o projeto

---
+++ src/test/java/com/application/teste/datacore/DataCoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.application.teste.datacore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DataCoreApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
